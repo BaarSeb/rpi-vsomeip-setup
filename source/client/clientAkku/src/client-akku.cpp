b7dc7aa30ab0660ec159fec6be8b92232c65cc4b
Sebastian Baar <sebastian.baar@gmx.de>
2019-01-13 19:07:25 +0100

    akku msg as event

--- source/client/clientAkku/src/client-akku.cpp
+++ source/client/clientAkku/src/client-akku.cpp
@@ -1,6 +1,7 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
+#include <chrono>
 
 #include <condition_variable>
 #include <thread>
@@ -10,26 +11,48 @@
 #define SAMPLE_SERVICE_ID 0x1234
 #define SAMPLE_INSTANCE_ID 0x5678
 #define SAMPLE_METHOD_ID 0x0421
+#define SAMPLE_EVENT_ID 0x3333
+#define SAMPLE_EVENTGROUP_ID 0x4444
 
 std::shared_ptr< vsomeip::application > app;
 std::mutex mutex;
 std::condition_variable condition;
 
+//void run() {
+//  std::unique_lock<std::mutex> its_lock(mutex);
+//  condition.wait(its_lock);
+//
+//  std::shared_ptr< vsomeip::message > request;
+//  request = vsomeip::runtime::get()->create_request();
+//  request->set_service(SAMPLE_SERVICE_ID);
+//  request->set_instance(SAMPLE_INSTANCE_ID);
+//  request->set_method(SAMPLE_METHOD_ID);
+//
+//  std::shared_ptr< vsomeip::payload > its_payload = vsomeip::runtime::get()->create_payload();
+//  std::vector< vsomeip::byte_t > its_payload_data;
+//  for(vsomeip::byte_t i=0; i<10; i++) {
+//	  its_payload_data.push_back(i % 256);
+//  }
+//  while(1){
+//    std::cout << "Sending akku request" << std::endl;
+//    its_payload->set_data(its_payload_data);
+//    request->set_payload(its_payload);
+//
+//    app->send(request, true);
+//    std::this_thread::sleep_for(std::chrono::seconds (5));
+//  }
+//}
+
+
 void run() {
   std::unique_lock<std::mutex> its_lock(mutex);
   condition.wait(its_lock);
 
-  std::shared_ptr< vsomeip::message > request;
-  request = vsomeip::runtime::get()->create_request();
-  request->set_service(SAMPLE_SERVICE_ID);
-  request->set_instance(SAMPLE_INSTANCE_ID);
-  request->set_method(SAMPLE_METHOD_ID);
+  std::set<vsomeip::eventgroup_t> its_groups;
+  its_groups.insert(SAMPLE_EVENTGROUP_ID);
+  app->request_event(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENT_ID, its_groups, true);
+  app->subscribe(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENTGROUP_ID);
 
-  while(1){
-    std::cout << "Sending akku request" << std::endl;
-    app->send(request, true);
-    std::this_thread::sleep_for(std::chrono::seconds (5));
-  }
 }
 
 void on_message(const std::shared_ptr<vsomeip::message> &_response) {
@@ -54,7 +77,7 @@ int main() {
     app->init();
     app->register_availability_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, on_availability);
     app->request_service(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID);
-    app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_message);
+    app->register_message_handler(vsomeip::ANY_SERVICE, vsomeip::ANY_INSTANCE, vsomeip::ANY_METHOD, on_message);
     std::thread sender(run);
     app->start();
 }
--- source/service/serviceAkku/src/service-akku.cpp
+++ source/service/serviceAkku/src/service-akku.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <fstream>
 #include <chrono>
+#include <thread>
 
 #include <vsomeip/vsomeip.hpp>
 #include "../../../../nlohmann/json.hpp"
@@ -10,6 +11,8 @@
 #define SAMPLE_SERVICE_ID 0x1234
 #define SAMPLE_INSTANCE_ID 0x5678
 #define SAMPLE_METHOD_ID 0x0421
+#define SAMPLE_EVENT_ID 0x3333
+#define SAMPLE_EVENTGROUP_ID 0x4444
 
 std::shared_ptr<vsomeip::application> app;
 
@@ -46,11 +49,46 @@ void on_message(const std::shared_ptr<vsomeip::message> &_request) {
   app->send(resp, true);
 }
 
+void run(){
+	while(1){
+		// Read in the json file and add timestamp
+		std::ifstream s("/etc/service_akku_msg.json");
+		json j;
+		s >> j;
+		auto t = std::time(nullptr);
+		auto tm = *std::localtime(&t);
+
+		std::ostringstream oss;
+		oss << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");
+		auto time = oss.str();
+
+		j["timestamp"] = time;
+
+		// Construct string to notify
+		std::string str = j.dump();
+
+		std::shared_ptr<vsomeip::payload> payload = vsomeip::runtime::get()->create_payload();
+		std::vector<vsomeip::byte_t> pl_data(str.begin(), str.end());
+		payload->set_data(pl_data);
+
+		std::set<vsomeip::eventgroup_t> its_groups;
+		its_groups.insert(SAMPLE_EVENTGROUP_ID);
+
+		app->notify(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENT_ID, payload);
+
+	    std::this_thread::sleep_for(std::chrono::seconds (5));
+	}
+}
+
 int main() {
+   std::set<vsomeip::eventgroup_t> its_groups;
+   its_groups.insert(SAMPLE_EVENTGROUP_ID);
 
    app = vsomeip::runtime::get()->create_application("service-akku");
    app->init();
    app->register_message_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_METHOD_ID, on_message);
    app->offer_service(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID);
+   app->offer_event(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, SAMPLE_EVENT_ID, its_groups, true);
+   std::thread sender(run);
    app->start();
 }
